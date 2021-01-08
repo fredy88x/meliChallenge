e2e8583b86a090a2bf6bde331d79db98dc1f09f2
fredy <fredy777x@gmail.com>
2021-01-07 22:10:38 -0500

    configuracion redis prod

--- src/main/java/com/challenge/meli/configurations/RedisConfig.java
+++ src/main/java/com/challenge/meli/configurations/RedisConfig.java
@@ -1,13 +1,18 @@
 package com.challenge.meli.configurations;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
 
 @Configuration
@@ -15,19 +20,28 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisConfig {
 	
 	
+	@SuppressWarnings("deprecation")
 	@Bean
 	RedisConnectionFactory jedisConnectionFactory() {
-		//RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redisdb.wmbjqy.ng.0001.use2.cache.amazonaws.com", 6379);
-        //return new JedisConnectionFactory(redisStandaloneConfiguration);
-		JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(10);
-        poolConfig.setMaxIdle(5);
-        poolConfig.setMinIdle(1);
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
-        poolConfig.setTestWhileIdle(true);
-        poolConfig.setMaxWaitMillis(10*1000);
-        return new JedisConnectionFactory(poolConfig);
+		String redistogoUrl = System.getenv("REDISTOGO_URL");
+		URI redistogoUri;
+		try {
+			redistogoUri = new URI(redistogoUrl);
+			JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+
+			jedisConnFactory.setUsePool(true);
+			jedisConnFactory.setHostName(redistogoUri.getHost());
+			jedisConnFactory.setPort(redistogoUri.getPort());
+			jedisConnFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
+			jedisConnFactory.setPassword(redistogoUri.getUserInfo().split(":", 2)[1]);
+
+			return jedisConnFactory;
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		
 	}
 	
 	@Bean
